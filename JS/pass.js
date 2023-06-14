35721d0cbcac9c66e107082f2346cdb8e07dec19
sandboxPrueba <126126049+sandboxPrueba@users.noreply.github.com>
2023-06-14 13:03:40 -0500

    Update pass.js

--- JS/pass.js
+++ JS/pass.js
@@ -5,7 +5,7 @@ function ingresar(){
  if(document.form.pass.value == contraseña && document.form.user.value ==usuario){
     console.log("contraseña correcta");
     localStorage.setItem("usuario",usuario);
-    window.location="pages/principal.html";
+    window.location="Pages/principal.html";
  }else{
    alert("contraseña incorrecta")
 }
